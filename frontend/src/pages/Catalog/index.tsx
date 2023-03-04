8674d07f04c138f3729b9db3d6113e1da03ed677
hstt-coder <eduardolopes19@outlook.com>
2023-03-04 19:01:05 -0300

    useState, useEffect and useParams

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -9,7 +9,7 @@ const Catalog = () => {
     id: 2,
     name: 'Smart TV',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis eligendi eveniet deleniti. Animi architecto vero quos. Minus exercitationem esse quisquam, perspiciatis culpa dolores nulla voluptas maiores. Repellat numquam eaque delectus hic similique, expedita quisquam. Repellat accusamus soluta nihil provident ea impedit?',
     price: 2190.0,
     imgUrl:
       'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,28 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 import './styles.css';
 
+
+type UrlParams = {
+  productId: string;
+}
+
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(BASE_URL + `/products/${productId}`).then((response) => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -17,53 +36,19 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2343.32} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Perspiciatis eligendi eveniet deleniti. Animi architecto vero
-                quos. Minus exercitationem esse quisquam, perspiciatis culpa
-                dolores nulla voluptas maiores. Repellat numquam eaque delectus
-                hic similique, expedita quisquam. Repellat accusamus soluta
-                nihil provident ea impedit? Ut reiciendis atque vel nostrum
-                aliquid quam eos, incidunt aspernatur ex deleniti impedit
-                quaerat beatae! Ratione qui consequatur perferendis repellendus
-                aut rem tenetur nesciunt dolorum ex exercitationem, fugiat
-                quaerat voluptatem consectetur nostrum et excepturi mollitia
-                tempore soluta magni, sint praesentium ipsa quod non! Tempore
-                itaque provident necessitatibus maxime, fuga ab dicta
-                accusantium accusamus alias adipisci quas consectetur expedita
-                suscipit repellendus distinctio nobis, delectus quisquam quasi
-                dolore officiis cum. Ab blanditiis deserunt optio eveniet autem
-                nostrum id alias ex? Porro beatae est quod voluptatem
-                exercitationem a labore itaque nesciunt, tempore at consectetur
-                dolore nobis quia hic ratione laudantium deserunt eos
-                consequuntur! Nostrum omnis dolores cum ullam mollitia, non ea
-                enim. Maiores, laboriosam omnis hic dignissimos adipisci
-                accusamus dolorum corrupti commodi porro alias perspiciatis
-                consequuntur aperiam libero nostrum reiciendis mollitia?
-                Deserunt ea consequuntur aut, quaerat consectetur architecto.
-                Maxime impedit voluptate porro, accusamus ullam sit vero
-                obcaecati error, minus amet consectetur eligendi ea eos
-                veritatis numquam perferendis neque sequi. A quis quas
-                voluptatum doloremque perspiciatis, cum harum nesciunt voluptas
-                voluptates quidem libero omnis porro suscipit ut tempora facere
-                repellendus laudantium perferendis cupiditate similique amet?
-                Labore facilis exercitationem, pariatur fuga repellendus eum
-                error deleniti porro laborum eius, modi natus doloribus maiores
-                reprehenderit, vero eligendi magni! Rem cupiditate quasi totam
-                nostrum consequatur quos odit!
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
