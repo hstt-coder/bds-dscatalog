edded0863cbf6bb35769ed1ebaeb605d7ceee8d8
hstt-coder <eduardolopes19@outlook.com>
2023-03-05 12:43:56 -0300

    Catalog listing

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,30 +1,33 @@
+import axios from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import { AxiosParams } from 'types/vendor/axios';
+import { SpringPage } from 'types/vendor/spring';
+import { BASE_URL } from 'util/requests';
+
 import './styles.css';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis eligendi eveniet deleniti. Animi architecto vero quos. Minus exercitationem esse quisquam, perspiciatis culpa dolores nulla voluptas maiores. Repellat numquam eaque delectus hic similique, expedita quisquam. Repellat accusamus soluta nihil provident ea impedit?',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'New name',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
       },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
-  };
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
 
   return (
     <div className="my-4 container catalog-container">
@@ -32,49 +35,16 @@ const Catalog = () => {
         <h1>Catálogo de Produtos</h1>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/2">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/3">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/4">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/5">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/6">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/7">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/8">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <ProductCard product={product} />
+            </Link>
+          </div>
+        ))}
       </div>
       <div className="row">
-        <Pagination/>
+        <Pagination />
       </div>
     </div>
   );
