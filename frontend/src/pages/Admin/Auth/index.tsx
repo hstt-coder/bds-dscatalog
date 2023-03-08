a01291dec6fbed89b6f6e3c2a333ee4ebe7c2b9f
hstt-coder <eduardolopes19@outlook.com>
2023-03-07 22:44:14 -0300

    Auth Routes

---
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,31 @@
+import {ReactComponent as AuthImage} from 'assets/images/auth-image.svg';
+import { Switch, Route} from 'react-router-dom';
+import './styles.css';
+
+
+const Auth = () => {
+  return (
+    <div className="auth-container">
+      <div className="auth-banner-container">
+        <h1>Divulge seus produtos no DS Catalog</h1>
+        <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
+        <AuthImage/>
+      </div>
+      <div className="auth-form-container">
+        <Switch>
+          <Route path="/admin/auth/login">
+            <h1>Card Login</h1>
+          </Route>
+          <Route path="/admin/auth/signup">
+            <h1>Card Sign-up</h1>
+          </Route>
+          <Route path="/admin/auth/recover">
+            <h1>Card Recover</h1>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
